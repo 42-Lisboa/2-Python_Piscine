41d250d7f60c4c2257b655209716a60955078722
Jonathan <jcas1808@gmail.com>
2026-07-15 20:35:22 +0100

    🚧 wip(ex3): start tracker system implementation

--- py03/ex3/ft_achievement_tracker.py
+++ py03/ex3/ft_achievement_tracker.py
@@ -7,27 +7,47 @@ import random
 
 def gen_player_achievements() -> set:
     achievements_list = [
-         "🛠️ Crafting Genius",
-         "🌍 World Savior",
-         "🗺️ Master Explorer",
-         "💎 Collector Supreme",
-         "🛡️ Untouchable",
-         "⚔️ Boss Slayer",
-         "⚒️ Forge Master",
-         "🕊️ Realm Liberator",
-         "🧭 Uncharted Pathfinder",
-         "🏺 Relic Hunter",
-         "🥷 Flawless Assassin",
-         "🐉 Dragon Bane",
-         "🧪 Alchemy Prodigy",
-         "🦸 Champion of Light",
-         "🏔️ Summit Conqueror",
-         "💰 Treasure Hoarder",
-         "👻 Phantom Dodger",
-         "👹 Monster Crusher",
-         "⚙️ Gadget Inventor",
-         "👑 Ultimate Legend"
+         "🛠️  Crafting Genius",
+         "🌍  World Savior",
+         "🗺️  Master Explorer",
+         "💎  Collector Supreme",
+         "🛡️  Untouchable",
+         "⚔️  Boss Slayer",
+         "⚒️  Forge Master",
+         "🕊️  Realm Liberator",
+         "🧭  Uncharted Pathfinder",
+         "🏺  Relic Hunter",
+         "🥷  Flawless Assassin",
+         "🐉  Dragon Bane",
+         "🧪  Alchemy Prodigy",
+         "🦸  Champion of Light",
+         "🏔️  Summit Conqueror",
+         "💰  Treasure Hoarder",
+         "👻  Phantom Dodger",
+         "👹  Monster Crusher",
+         "⚙️  Gadget Inventor",
+         "👑  Ultimate Legend"
     ]
     number_set = random.randint(1, 10)
     achievements_set = set(random.sample(achievements_list, k=number_set))
     return achievements_set
+
+
+# ====================== Achievements Tracker System =========================
+
+def achievements_tracker_system() -> None:
+    print("\n============= ⚔️  Achievements Tracker System ⚔️ =============\n")
+
+    players = ["Alice", "Bob", "Charlie", "Dylan"]
+    for player in players:
+        print(f"🎮 Player {player}: {gen_player_achievements()}")
+
+    achievements_union = 
+
+
+
+
+# ============================== Program Test ================================
+
+if __name__ == "__main__":
+    achievements_tracker_system()
