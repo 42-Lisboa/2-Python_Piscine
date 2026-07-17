747c00222971bdfbe27a65fb08ad7222fa96fdaf
Jonathan Alves <36007003-ja.de@users.noreply.gitlab.com>
2026-07-17 03:05:08 +0100

    ✨ feat(ex4): finish exercise implementation with all system analysis done

--- py03/ex4/ft_inventory_system.py
+++ py03/ex4/ft_inventory_system.py
@@ -16,7 +16,7 @@ def fill_inventory(items_input: list) -> dict:
         temp = item.split(':')
 
         if len(temp) != 2:
-            print(f"❌ Error - invalid parameter {temp}")
+            print(f"❌ Error - invalid parameter '{item}'")
             continue
         if temp[0] in inventory:
             print(f"❌ Redundant item '{temp[0]}' - discarding")
@@ -29,14 +29,42 @@ def fill_inventory(items_input: list) -> dict:
     return inventory
 
 
-# ====================== Inventory System Function =========================
+# ========================= Inventory System Function =========================
 
 def inventory_system_analysis(items_input: list) -> None:
-    inventory = fill_inventory(items_input)
+    inv = fill_inventory(items_input)
 
-    print("\n============= ⚔️  Inventory System Analysis ⚔️  ============\n")
-    print(f"Got inventory: {inventory}")
-    print(f"Item list: {list(inventory.keys())}")
+    print("\n============== ⚔️  Inventory System Analysis ⚔️  =============\n")
+    print(f"✅ Got inventory: {inv}")
+    print(f"📜 Item list: {list(inv.keys())}")
+
+    total_qty = sum(inv.values())
+    print(f"🧮 Total quantity of the {len(inv)} items: {total_qty}")
+
+    for item, quantity in inv.items():
+        item_weight = (quantity / total_qty) * 100
+        print(f"  📊 Item {item} represents\t{(item_weight):.1f}%")
+
+    max_val = max(inv.values())
+    max_key = ""
+    for item, quantity in inv.items():
+        if quantity == max_val:
+            max_key = item
+            break
+    min_val = min(inv.values())
+    min_key = ""
+    for item, quantity in inv.items():
+        if quantity == min_val:
+            min_key = item
+            break
+    # Another way to use the max and min with the key and get function
+    # max_item = max(inv, key=inv.get)  # type: ignore
+    # min_item = min(inv, key=inv.get)  # type: ignore
+    print(f"⬆️  Item most abundant: {max_key} with quantity {max_val}")
+    print(f"⬇️  Item least abundant: {min_key} with quantity {min_val}")
+
+    inv["magic_item"] = 1
+    print(f"✨ Updated inventory: {inv}\n")
 
 
 # ============================== Program Test ================================
